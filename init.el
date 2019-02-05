c0aca194b697326bc7df7af02ac8ce5af416b5f8
MatthewZMD <matthewzmd@gmail.com>
2019-02-04 23:25:48 -0500

    initial commit of init.el file

---
+++ init.el
@@ -0,0 +1,193 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+			 ("melpa stable" . "http://stable.melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit tide web-mode-edit-element popup-kill-ring bookmark+ 2048-game format-all flx counsel ivy avy smex auto-complete system-packagese which-key use-package doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; Minimal UI
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+;; Maximize the frame
+(toggle-frame-maximized)
+
+;; Eval-buffer ELisp Code
+(global-set-key (kbd "<f5>") 'eval-buffer)
+
+
+;; Column Number
+(global-linum-mode t)
+
+
+;; Setup theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-vibrant t))
+
+;; Setup Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+;; Setup org mode
+;; (setq org-startup-indented t)
+;; (setq org-startup-folded "showall")
+;; (setq org-directory "~/org")
+
+
+;; Setup Avy
+(global-set-key (kbd "C-c C-SPC") 'avy-goto-char-timer)
+(global-set-key (kbd "C-c C-l") 'avy-goto-line)
+
+;; Setup Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; FlyCheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Setup Ivy
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "[%d/%d] ")
+;; Ivy-based interface to standard commands
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; Ivy-based interface to shell and system tools
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; Fuzzy matching
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-plus)))
+
+
+;; Auto-rename new eww buffers
+(defun xah-rename-eww-hook ()
+  "Rename eww browser's buffer so sites open in new page."
+  (rename-buffer "eww" t))
+(add-hook 'eww-mode-hook #'xah-rename-eww-hook)
+
+;; C-u M-x eww will force a new eww buffer
+(defun modi/force-new-eww-buffer (orig-fun &rest args)
+  "When prefix argument is used, a new eww buffer will be created,
+regardless of whether the current buffer is in `eww-mode'."
+  (if current-prefix-arg
+      (with-temp-buffer
+        (apply orig-fun args))
+    (apply orig-fun args)))
+(advice-add 'eww :around #'modi/force-new-eww-buffer)
+
+;; Auto-complete
+(global-auto-complete-mode t)
+
+;; Auto-format all buffer
+(format-all-mode)
+
+;; Setup Popup-kill-ring
+(global-set-key (kbd "M-y") 'popup-kill-ring)
+
+;; Setup Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+
+;; Setup Bookmark+
+;; (add-to-list 'load-path "~/.emacs.d/lisp/bookmark-plus")
+;; (require 'bookmark+)
+
+;; Setup Web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+
+
+;; Dired - Handle zip compression
+(eval-after-load "dired-aux"
+  '(add-to-list 'dired-compress-file-suffixes
+                '("\\.zip\\'" ".zip" "unzip")))
+
+;; Setup Tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; (add-hook 'web-mode-hook
+;;           (lambda ()
+;;             (when (string-equal "tsx" (file-name-extension buffer-file-name))
+;;               (setup-tide-mode))))
+;; ;; enable typescript-tslint checker
+;; (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+
+(provide 'init)
+;;; init.el ends here
