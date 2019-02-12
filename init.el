027e02aacbbc634c9eb3ee63ff9142d633ad37fe
Zeng <if994190@canada01.ifdsnet.int>
2019-02-12 16:08:00 -0500

    Custom set variables moved to init.el

--- init.el
+++ init.el
@@ -9,8 +9,6 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
 (garbage-collect)
 
-(provide 'init)
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,3 +23,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(dashboard-banner-logo-title-face ((t (:family "Love LetterTW" :height 125)))))
+
+(provide 'init)
+;;; init.el ends here
