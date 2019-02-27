80ffa79ca55f2a0b035871b2bf114d6b0907ac33
Mingde Zeng <matthewzmd@gmail.com>
2019-02-27 13:16:37 -0500

    Minor changes

--- custom-file.el
+++ custom-file.el
@@ -1,14 +1,3 @@
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(column-number-mode t)
-;;  '(package-selected-packages
-;; 	 (quote
-;; 		(amx doom-modeline auto-package-update pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode smartparens dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs-icons-dired treemacs projectile magit discover-my-major dimmer undo-tree popup-kill-ring which-key smooth-scrolling counsel ivy avy ag diminish toc-org org-bullets dashboard doom-themes use-package)))
-;;  '(smooth-scrolling-mode t))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,6 +6,7 @@
  '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
  '(dashboard-banner-logo-title-face ((t (:family "Love LetterTW" :height 125))))
  '(font-lock-comment-face ((t (:foreground "#828282")))))
+ 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
