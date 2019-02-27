c073ba6b8672d296e63608d5be0e7533e814b9e9
Mingde Zeng <matthewzmd@gmail.com>
2019-02-27 13:06:13 -0500

    200 Commits! Major changes include readme redesign, modeline, icons, winner mode, ivy

--- custom-file.el
+++ custom-file.el
@@ -1,11 +1,14 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auto-package-update pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode smartparens dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs-icons-dired treemacs projectile magit discover-my-major dimmer undo-tree popup-kill-ring which-key smooth-scrolling counsel ivy smex avy ag diminish toc-org org-bullets smart-mode-line dashboard doom-themes use-package))))
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(column-number-mode t)
+;;  '(package-selected-packages
+;; 	 (quote
+;; 		(amx doom-modeline auto-package-update pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode smartparens dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs-icons-dired treemacs projectile magit discover-my-major dimmer undo-tree popup-kill-ring which-key smooth-scrolling counsel ivy avy ag diminish toc-org org-bullets dashboard doom-themes use-package)))
+;;  '(smooth-scrolling-mode t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,3 +17,11 @@
  '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
  '(dashboard-banner-logo-title-face ((t (:family "Love LetterTW" :height 125))))
  '(font-lock-comment-face ((t (:foreground "#828282")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	 (quote
+		(pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode smartparens dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs-icons-dired treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
