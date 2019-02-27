4e84877d9a046338c2eb386c6310c990cc45d3b0
Mingde Zeng <matthewzmd@gmail.com>
2019-02-27 15:46:45 -0500

    Added icons for dired and ivy

--- custom-file.el
+++ custom-file.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(all-the-icons-dired-dir-face ((t (\` (:foreground (\, (face-background (quote default))))))))
  '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
  '(dashboard-banner-logo-title-face ((t (:family "Love LetterTW" :height 125))))
  '(font-lock-comment-face ((t (:foreground "#828282")))))
@@ -14,4 +15,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
 	 (quote
-		(pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode smartparens dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs-icons-dired treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
+		(all-the-icons-ivy all-the-icons-dired pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode smartparens dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
