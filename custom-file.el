5f894fcceda5635f20ec4bf216ee2dca11d20850
Mingde Zeng <matthewzmd@gmail.com>
2019-02-28 12:13:50 -0500

    Change from tabs to spaces
    This reverts commit 207638fc2bd6c532da9156ccc9123071690f0dba.

--- custom-file.el
+++ custom-file.el
@@ -7,12 +7,12 @@
  '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
  '(dashboard-banner-logo-title-face ((t (:family "Love LetterTW" :height 125))))
  '(font-lock-comment-face ((t (:foreground "#828282")))))
- 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 (quote
-		(all-the-icons-ivy all-the-icons-dired pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
+   (quote
+    (doom-modeline- all-the-icons-ivy all-the-icons-dired pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
