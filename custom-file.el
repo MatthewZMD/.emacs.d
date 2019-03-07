1729f9a80d3af453732f278028b0cd9f6efe11b9
Mingde Zeng <matthewzmd@gmail.com>
2019-03-07 15:03:53 -0500

    Added highlight indent guides

--- custom-file.el
+++ custom-file.el
@@ -5,10 +5,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dashboard-banner-logo-title ((t (:family "Love LetterTW" :height 125))))
  '(all-the-icons-dired-dir-face ((t (\` (:foreground (\, (face-background (quote default))))))))
  '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
  '(cursor ((t (:background "BlanchedAlmond"))))
+ '(dashboard-banner-logo-title ((t (:family "Love LetterTW" :height 125))))
  '(font-lock-comment-face ((t (:foreground "#828282")))))
 
 (custom-set-variables
@@ -18,4 +18,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (solaire-mode smartparens all-the-icons-ivy all-the-icons-dired pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
+    (highlight-indent-guides solaire-mode smartparens all-the-icons-ivy all-the-icons-dired pacmacs 2048-game speed-type company-arduino arduino-mode company-jedi jedi elpy company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish smooth-scrolling dashboard doom-modeline doom-themes auto-package-update use-package))))
