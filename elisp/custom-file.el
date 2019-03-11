564d26f5e73e578e8055cecc5d627161e7945d25
Zeng <if994190@canada01.ifdsnet.int>
2019-03-11 13:32:16 -0400

    Added smaller picture. Removed Solaire mode

--- elisp/custom-file.el
+++ elisp/custom-file.el
@@ -10,7 +10,6 @@
  '(cursor ((t (:background "BlanchedAlmond"))))
  '(dashboard-banner-logo-title ((t (:family "Love LetterTW" :height 125))))
  '(font-lock-comment-face ((t (:foreground "#828282")))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,4 +17,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (lsp-java-treemacs dap-java lsp-java dap-mode company-lsp lsp-ui lsp-mode highlight-indent-guides solaire-mode smartparens all-the-icons-ivy all-the-icons-dired pacmacs 2048-game speed-type company-arduino arduino-mode company-irony-c-headers company-irony irony tide typescript-mode js2-mode emmet-mode web-mode dumb-jump format-all flycheck company treemacs-projectile treemacs-magit treemacs projectile magit discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag toc-org org-bullets dimmer diminish dashboard doom-modeline doom-themes auto-package-update use-package))))
+    (pacmacs 2048-game speed-type toc-org org-bullets tide typescript-mode js2-mode emmet-mode web-mode company-arduino arduino-mode company-irony-c-headers company-irony irony lsp-java dap-mode company-lsp lsp-ui lsp-mode highlight-indent-guides format-all smartparens dumb-jump flycheck company treemacs-projectile treemacs-magit treemacs projectile magit dimmer diminish dashboard doom-modeline doom-themes all-the-icons-ivy all-the-icons-dired all-the-icons discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag auto-package-update use-package))))
