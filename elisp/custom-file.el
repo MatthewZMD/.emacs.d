c9d8db4ec133bf580c6cebcc6569b2d57aaf5d21
Matthew Zeng <matthewzmd@gmail.com>
2019-03-13 16:22:44 -0400

    Supported md export to org

--- elisp/custom-file.el
+++ elisp/custom-file.el
@@ -9,12 +9,15 @@
  '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
  '(cursor ((t (:background "BlanchedAlmond"))))
  '(dashboard-banner-logo-title ((t (:family "Love LetterTW" :height 120))))
- '(font-lock-comment-face ((t (:foreground "#828282")))))
+ '(font-lock-comment-face ((t (:foreground "#828282"))))
+ '(lsp-ui-doc-background ((t (:background nil))))
+ '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(package-selected-packages
    (quote
     (htmlize 2048-game speed-type toc-org tide typescript-mode js2-mode emmet-mode web-mode company-arduino arduino-mode company-irony-c-headers company-irony irony lsp-java dap-mode company-lsp lsp-ui lsp-mode highlight-indent-guides format-all smartparens dumb-jump flycheck company treemacs-projectile treemacs-magit treemacs projectile magit dimmer diminish dashboard doom-modeline doom-themes all-the-icons-ivy all-the-icons-dired all-the-icons discover-my-major undo-tree popup-kill-ring which-key counsel amx ivy avy ag auto-package-update use-package))))
