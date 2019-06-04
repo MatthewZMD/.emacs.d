a35686471171d666b50a7d5e3288c295316a84f0
Mingde Zeng <matthewzmd@gmail.com>
2019-06-03 23:50:36 -0400

    hide menu-bar-mode in all conditions

--- init.el
+++ init.el
@@ -52,8 +52,8 @@
     (progn
       (scroll-bar-mode -1)
       (tool-bar-mode   -1)
-      (tooltip-mode    -1)
-      (menu-bar-mode   -1)))
+      (tooltip-mode    -1)))
+(menu-bar-mode   -1)
 ;; -DisableUnnecessaryInterface
 
 ;; AvoidStartupGarbageCollect
