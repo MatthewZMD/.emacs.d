3fa71dc346c246a8e34e4a5da105bfcb2e3cb9c7
Matthew Zeng <matthewzmd@gmail.com>
2019-06-09 21:42:02 -0400

    Use doom-Iosvkem theme

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -55,7 +55,7 @@
     (doom-themes-visual-bell-config)
     ;; Corrects (and improves) org-mode's native fontification.
     (doom-themes-org-config)
-    (load-theme 'doom-dracula t)))
+    (load-theme 'doom-Iosvkem t)))
 ;; -DoomThemes
 
 ;; DoomModeline
