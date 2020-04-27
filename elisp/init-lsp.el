67a09ad4ff8af7db6747d71f367532d2459892ce
Mingde (Matthew) Zeng <matthewzmd@gmail.com>
2020-04-27 09:42:13 -0400

    Disable lsp-eldoc-hook

--- elisp/init-lsp.el
+++ elisp/init-lsp.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 10:42:09 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Mon Mar  9 13:07:21 2020 (-0400)
+;; Last-Updated: Mon Apr 27 09:40:49 2020 (-0400)
 ;;           By: Mingde (Matthew) Zeng
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d lsp
@@ -49,6 +49,7 @@
   (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
   (lsp-file-watch-threshold 2000)
   (read-process-output-max (* 1024 1024))
+  (lsp-eldoc-hook nil)
   :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
   :hook ((java-mode python-mode go-mode
           js-mode js2-mode typescript-mode web-mode
