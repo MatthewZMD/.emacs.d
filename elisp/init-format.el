b79e7e5f05001ee6cb64764097635351f2e4dfa1
Mingde (Matthew) Zeng <matthewzmd@posteo.net>
2022-09-21 21:49:55 -0400

    Add exec-path-from-shell and ediff modifications
    Signed-off-by: Mingde (Matthew) Zeng <matthewzmd@posteo.net>

--- elisp/init-format.el
+++ elisp/init-format.el
@@ -40,6 +40,13 @@
   :bind ("C-c C-f" . format-all-buffer))
 ;; -FormatAllPac
 
+;; EdiffPac
+(use-package ediff
+  :custom
+  (ediff-split-window-function #'split-window-horizontally)
+  (ediff-window-setup-function #'ediff-setup-windows-plain))
+;; -EdiffPac
+
 (provide 'init-format)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init-format.el ends here
--- elisp/init-shell.el
+++ elisp/init-shell.el
@@ -94,6 +94,13 @@
   :config (term-keys-mode t))
 ;; -TermKeysPac
 
+;; ExecPathFromShellPac
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns x))
+  :config
+  (exec-path-from-shell-initialize))
+;; -ExecPathFromShellPac
+
 (provide 'init-shell)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init-shell.el ends here
